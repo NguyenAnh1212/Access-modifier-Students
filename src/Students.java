bc7b18e13ca181ed04472116bd0d1e54d5a06ff5
unknown <anhnt.tb12@gmail.com>
2022-02-11 14:52:52 +0700

    students

---
+++ src/Students.java
@@ -0,0 +1,23 @@
+public class Students {
+    private String name = "Jonh";
+    private String classes = "C02";
+
+    public Students() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+}
---
+++ src/TestStudents.java
@@ -0,0 +1,8 @@
+public class TestStudents {
+    public static void main(String[] args) {
+        Students s1 = new Students();
+      s1.setName("Ánh");
+      s1.setClasses("C1221H1");
+        System.out.println("Học Sinh " + s1.getName() + " học lớp " + s1.getClasses());
+    }
+}
